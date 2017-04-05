3496bc3c6c205bfc732837d00506a807fe971de4
Ryan Spangler <ryan.spangler@gmail.com>
2017-04-05 00:54:32 -0700

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/caribou/polaris"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.3.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-codec "1.0.1"]
                  [clout "1.1.0"]])
