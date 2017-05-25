66b283e8e5339d00b44d71a27cadbf8c6fd63bab
Ryan Spangler <ryan.spangler@gmail.com>
2017-05-25 00:24:36 -0700

    ring

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject polaris "0.0.18"
+(defproject polaris "0.0.19"
   :description "Requests guided by the constellations"
   :url "http://github.com/caribou/polaris"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-codec "1.0.1"]
+                 [ring/ring-core "1.6.0"]
                  [clout "1.1.0"]])
